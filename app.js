27d67cb54966eef16fec2ee55bcdef6501b5080b
Evan James <thevan777@gmail.com>
2021-09-28 13:29:03 -0400

    modified app to push message to message.txt

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    const url = req.url;
+    const method = req.method
+
+    if (url === '/') {
+        res.write(`
+        <html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message">
+                    <button type="submit">Send</button>
+                </form>
+            </body>
+        </html>
+    `)
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody)
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, (err) => {
+                res.writeHead(302, {
+                    'Location': '/'
+                })
+                return res.end()
+            })
+        })
+        
+        
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`
+        <html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                <h1>Other</h1>
+            </body>
+        </html>
+    `)
+    res.end()
 });
 
 server.listen(3000)
